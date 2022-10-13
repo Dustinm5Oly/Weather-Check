ec4733db7e2f5115a0dc0b3a22149367f142fac1
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-10-13 16:57:38 -0700

    Able to finish JS file and rearange the html document

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 const searchBtn = document.getElementById("search-city");
 const WeatherApiKey = `0b6c4873ea51e81471ca3b4b3cc3cc35`;
-const CityName = document.getElementById("selectedCity");
-const searchHistory=[];
-const searchForm = document.querySelector("#searchForm")
-const searchInput = document.querySelector("#input-group")
-const today = document.querySelector("#selectedCity")
-const forecast = document.querySelector("#nextFiveDays")
-const searchList= document.querySelector("#previouslySearched")
+let searchHistory=[];
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const today = document.querySelector("#today");
+const forecast = document.querySelector("#forecast");
+const searchList= document.querySelector("#history");
+const weatherApiRootUrl= "https://api.openweathermap.org";
 
 //I need to display user input to display city in previously searched
 function DisplayHistory(){
@@ -113,6 +113,8 @@ function renderForecastCard(forecast){
 function renderForecast(dailyForecast){
     let start= moment().add(1, "days").calendar();
     let end = moment().add(6,"days").calendar();
+    console.log(start);
+    console.log(end);
     let headingCol = document.createElement("div");
     let heading = document.createElement("h4");
     headingCol.setAttribute("class", "col-12");
@@ -121,115 +123,88 @@ function renderForecast(dailyForecast){
     forecast.innerHTML="";
     forecast.append(headingCol);
 
-    for (let index = 0; index < dailyForecast.length; index++) {
-        if (dailyForecast[i]>=start && dailyForecast[i]<end){
-        renderForecastCard(dailyForecast[i]);
+    for (let i = 0; i < dailyForecast.length; i++) {
+        if (dailyForecast[i].dt>=start && dailyForecast[i].dt<end){
+            // if (dailyForecast[i].dt_txt.slice(11,13)=="12"){
+                renderForecastCard(dailyForecast[i]);
+            // }
+        
         }
     }
 };
-
-
-
-
-// append body to card
-//set textContent to temp-${temp}
-//click event listeners
-
-
-//I need to display from API the city searched with city name, date, temp, wind, humidity and uv index
-
-//I need to display the next 5 days with the date, icon, temp, wind and humidity
-
-
-function cityDisplay(){
-    fetch(``)
+function renderItems(city, data) {
+    renderCurrentWeather(city, data.list[0]);
+    renderForecast(data.list);
 }
+function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+  
+    var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${WeatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        renderItems(city, data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+}
+function fetchCoords(search) {
+    var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${WeatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        if (!data[0]) {
+          alert('Location not found');
+        } else {
+          appendHistory(search);
+          fetchWeather(data[0]);
+        }
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+// function cityDisplay(){
+//     fetch(``)
+// }
+
+function handleSearchFormSubmit(e) {
+    // Don't continue if there is nothing in the search form
+    if (!searchInput.value) {
+      return;
+    }
+  
+    e.preventDefault();
+    var search = searchInput.value.trim();
+    fetchCoords(search);
+    searchInput.value = '';
+  }
+  
+  function handleSearchHistoryClick(e) {
+    // Don't do search if current elements is not a search history button
+    if (!e.target.matches('.btn-history')) {
+      return;
+    }
+  
+    var btn = e.target;
+    var search = btn.getAttribute('data-search');
+    fetchCoords(search);
+  }
+  
+  getHistory();
+  searchForm.addEventListener('submit', handleSearchFormSubmit);
+  searchList.addEventListener('click', handleSearchHistoryClick);
+  
+  
 
 
-
-// <div class="col-lg-9 pb-3">
-//           <section
-//             id="today"
-//             class="mt-3"
-//             role="region"
-//             aria-live="polite"
-//           ></section>
-//           <section
-//             id="forecast"
-//             class="row mt-3"
-//             role="region"
-//             aria-live="polite"
-//           ></section>
-
-
-// function renderItems(city, data) {
-//     renderCurrentWeather(city, data.list[0], data.city.timezone);
-//     renderForecast(data.list);
-//   }
-// function fetchWeather(location) {
-//     var { lat } = location;
-//     var { lon } = location;
-//     var city = location.name;
-  
-//     var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
-  
-//     fetch(apiUrl)
-//       .then(function (res) {
-//         return res.json();
-//       })
-//       .then(function (data) {
-//         renderItems(city, data);
-//       })
-//       .catch(function (err) {
-//         console.error(err);
-//       });
-//   }
-  
-// function fetchCoords(search) {
-//     var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
-  
-//     fetch(apiUrl)
-//       .then(function (res) {
-//         return res.json();
-//       })
-//       .then(function (data) {
-//         if (!data[0]) {
-//           alert('Location not found');
-//         } else {
-//           appendToHistory(search);
-//           fetchWeather(data[0]);
-//         }
-//       })
-//       .catch(function (err) {
-//         console.error(err);
-//       });
-//   }
-
-// function handleSearchFormSubmit(e) {
-//     // Don't continue if there is nothing in the search form
-//     if (!searchInput.value) {
-//       return;
-//     }
-  
-//     e.preventDefault();
-//     var search = searchInput.value.trim();
-//     fetchCoords(search);
-//     searchInput.value = '';
-//   }
-  
-//   function handleSearchHistoryClick(e) {
-//     // Don't do search if current elements is not a search history button
-//     if (!e.target.matches('.btn-history')) {
-//       return;
-//     }
-  
-//     var btn = e.target;
-//     var search = btn.getAttribute('data-search');
-//     fetchCoords(search);
-//   }
-  
-//   initSearchHistory();
-//   searchForm.addEventListener('submit', handleSearchFormSubmit);
-//   searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);
-  
-  
